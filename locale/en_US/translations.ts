fea09d689451e44310ba2edc4e1ed56ad2ce8370
1hitsong <3330318+1hitsong@users.noreply.github.com>
2022-07-14 09:01:00 -0400

    Add support for skip intro

--- locale/en_US/translations.ts
+++ locale/en_US/translations.ts
@@ -688,5 +688,9 @@
         <translation>Hides all clocks in Jellyfin. Jellyfin will need to be closed and reopened for change to take effect.</translation>
         <extracomment>Settings Menu - Description for option</extracomment>
     </message>
+    <message>
+        <source>Skip Intro</source>
+        <translation>Skip Intro</translation>
+    </message>
 </context>
 </TS>
